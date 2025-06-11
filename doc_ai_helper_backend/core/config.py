320de83d797e49fd3d99071283c3ab642c3efcc4
toruinaba <inaba.tooru@takenaka.co.jp>
2025-06-11 13:40:49 +0900

    Formatting(black)

--- doc_ai_helper_backend/core/config.py
+++ doc_ai_helper_backend/core/config.py
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
     model_config = {
         "env_file": ".env",
         "env_file_encoding": "utf-8",
-        "case_sensitive": False
+        "case_sensitive": False,
     }
 
 
